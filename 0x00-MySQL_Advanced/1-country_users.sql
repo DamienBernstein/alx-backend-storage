e73dabcc8fbc4b68833c562bfbb2bddea2a5808a
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-07-13 14:43:42 +0200

    Update 1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- script that creates a table with these following fields
 -- id, email, name, country
-CREATE TABLE IF NOT EXISITS users (
+CREATE TABLE IF NOT EXISTS users (
         id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
         email varchar(255) NOT NULL UNIQUE,
         name varchar(255),
